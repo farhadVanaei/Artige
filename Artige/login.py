422aa0e4cd41596044f41dfaa6006568d71aea7b
mohammad <mohammad.kajbaf@nak-mci.ir>
2018-12-25 21:23:06 +0330

    login file

--- Artige/login.py
+++ Artige/login.py
@@ -1,17 +1,20 @@
-import json
 import codecs
 import datetime
-import os.path
+import json
 import logging
+import os.path
+
 # import argparse
 try:
+    raise ImportError()
     from instagram_private_api import (
         Client, ClientError, ClientLoginError,
         ClientCookieExpiredError, ClientLoginRequiredError,
         __version__ as client_version)
 except ImportError:
     import sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../instagram_private_api'))
     from instagram_private_api import (
         Client, ClientError, ClientLoginError,
         ClientCookieExpiredError, ClientLoginRequiredError,
@@ -38,11 +41,17 @@ def onlogin_callback(api, new_settings_file):
         print('SAVED: {0!s}'.format(new_settings_file))
 
 
-if __name__ == '__main__':
-
+def login(args):
     logging.basicConfig()
-    logger = logging.getLogger('instagram_private_api')
-    logger.setLevel(logging.WARNING)
+    _logger = logging.getLogger('instagram_private_api')
+    if args.debug:
+        _logger.setLevel(logging.DEBUG)
+    else:
+        _logger.setLevel(logging.WARNING)
+    # s_handler = logging.StreamHandler()
+    # _logger.addHandler(s_handler)
+    if args.log_file_path:
+        _logger.addHandler(logging.FileHandler(args.log_file_path))
 
     # Example command:
     # python examples/savesettings_logincallback.py -u "yyy" -p "zzz" -settings "test_credentials.json"
@@ -51,10 +60,6 @@ if __name__ == '__main__':
     # parser.add_argument('-u', '--username', dest='username', type=str, required=True)
     # parser.add_argument('-p', '--password', dest='password', type=str, required=True)
     # parser.add_argument('-debug', '--debug', action='store_true')
-    from private import Config as args
-
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
 
     print('Client version: {0!s}'.format(client_version))
 
@@ -105,8 +110,46 @@ if __name__ == '__main__':
     cookie_expiry = api.cookie_jar.auth_expires
     print('Cookie Expiry: {0!s}'.format(datetime.datetime.fromtimestamp(cookie_expiry).strftime('%Y-%m-%dT%H:%M:%SZ')))
 
-    # Call the api
-    results = api.tag_search('cats')
-    assert len(results.get('results', [])) > 0
+    return api
 
-    print('All ok')
+
+if __name__ == '__main__':
+    from private import Config as args
+    api = login(args)
+
+    # Call the api:
+    rank_token = api.generate_uuid()
+    results = api.tag_search('cats', rank_token)
+    assert len(results.get('results', [])) > 0
+    import ipdb; ipdb.trace()
+    print('All ok\n results:\t', results)
+
+    # ---------- Pagination with max_id ----------
+    user_id = '2958144170'
+    followers = []
+    results = api.user_followers(user_id, rank_token)
+    followers.extend(results.get('users', []))
+
+    next_max_id = results.get('next_max_id')
+    while next_max_id:
+        results = api.user_followers(user_id, rank_token, max_id=next_max_id)
+        followers.extend(results.get('users', []))
+        if len(followers) >= 600:  # get only first 600 or so
+            break
+        next_max_id = results.get('next_max_id')
+
+    followers.sort(key=lambda x: x['pk'])
+    # print list of user IDs
+    print(json.dumps([u['pk'] for u in followers], indent=2))
+
+    # ---------- Pagination with rank_token and exclusion list ----------
+    rank_token = Client.generate_uuid()
+    has_more = True
+    tag_results = []
+    while has_more and rank_token and len(tag_results) < 60:
+        results = api.tag_search(
+            'cats', rank_token, exclude_list=[t['id'] for t in tag_results])
+        tag_results.extend(results.get('results', []))
+        has_more = results.get('has_more')
+        rank_token = results.get('rank_token')
+    print(json.dumps([t['name'] for t in tag_results], indent=2))
